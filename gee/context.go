9c16f7bb9b1cdce9806b807ebb54d3646a944d0b
tianye0718 <tianye0718@gmail.com>
2022-04-17 11:59:56 +1200

    Day5 - Middleware

--- gee/context.go
+++ gee/context.go
@@ -18,6 +18,9 @@ type Context struct {
 	Params map[string]string
 	// response info
 	StatusCode int
+	// middleware
+	handlers []HandlerFunc
+	index    int
 }
 
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
@@ -26,11 +29,25 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		Req:    req,
 		Path:   req.URL.Path,
 		Method: req.Method,
+		index:  -1,
 	}
 }
 
+func (c *Context) Next() {
+	c.index++
+	s := len(c.handlers)
+	for ; c.index < s; c.index++ {
+		c.handlers[c.index](c)
+	}
+}
+
+func (c *Context) Fail(code int, err string) {
+	c.index = len(c.handlers)
+	c.JSON(code, H{"message": err})
+}
+
 func (c *Context) Param(key string) string {
-	value, _ := c.Params[key]
+	value := c.Params[key]
 	return value
 }
 
--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/tianye0718/go-web/gee"
 )
 
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		// start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate processing time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
 func main() {
 
 	r := gee.New()
+	r.Use(gee.Logger())
 	r.GET("/index", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
@@ -24,6 +38,7 @@ func main() {
 		})
 	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
 	{
 		v2.GET("/hello/:name", func(c *gee.Context) {
 			// expect /hello/yetian
